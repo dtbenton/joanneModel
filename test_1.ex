daebd23c74f15552a65dd441304243f8795d1506
Deon Benton <dbenton2@swarthmore.edu>
2022-02-01 16:08:50 -0500

    s

---
+++ test_1.ex
@@ -0,0 +1,41 @@
+name: TestExample1
+I:
+(Shape)
+0 0 0 0 0 0 0 0 1
+(Color)
+0 0 0 0 0 0 0 0 1
+(Feature)
+1 0
+(Motion)
+0 1
+T:
+(Shape_OUT)
+0 0 0 0 0 0 0 0 1
+(Color_OUT)
+0 0 0 0 0 0 0 0 1
+(Feature_OUT)
+1 0
+(Motion_OUT)
+0 1
+;
+
+name: TestExample2
+I:
+(Shape)
+0 0 0 0 0 0 0 0 1
+(Color)
+0 0 0 0 0 0 0 0 1
+(Feature)
+1 0
+(Motion)
+1 0
+T:
+(Shape_OUT)
+0 0 0 0 0 0 0 0 1
+(Color_OUT)
+0 0 0 0 0 0 0 0 1
+(Feature_OUT)
+1 0
+(Motion_OUT)
+1 0
+;
---
+++ train_ID16.ex
@@ -0,0 +1,168 @@
+name: FirstExample
+I:
+(Shape)
+0 1 0 0 0 0 0 0 0
+(Color)
+0 1 0 0 0 0 0 0 0
+(Feature)
+1 0
+(Motion)
+1 0
+T:
+(Shape_OUT)
+0 1 0 0 0 0 0 0 0
+(Color_OUT)
+0 1 0 0 0 0 0 0 0
+(Feature_OUT)
+1 0
+(Motion_OUT)
+1 0
+;
+
+name: SecondExample
+I:
+(Shape)
+0 0 0 1 0 0 0 0 0
+(Color)
+0 0 0 1 0 0 0 0 0
+(Feature)
+0 1
+(Motion)
+0 1
+T:
+(Shape_OUT)
+0 0 0 1 0 0 0 0 0
+(Color_OUT)
+0 0 0 1 0 0 0 0 0
+(Feature_OUT)
+0 1
+(Motion_OUT)
+0 1
+;
+
+name: ThirdExample
+I:
+(Shape)
+1 0 0 0 0 0 0 0 0
+(Color)
+1 0 0 0 0 0 0 0 0
+(Feature)
+1 0
+(Motion)
+1 0
+T:
+(Shape_OUT)
+1 0 0 0 0 0 0 0 0
+(Color_OUT)
+1 0 0 0 0 0 0 0 0
+(Feature_OUT)
+1 0
+(Motion_OUT)
+1 0
+;
+
+name: FourthExample
+I:
+(Shape)
+0 0 1 0 0 0 0 0 0
+(Color)
+0 0 1 0 0 0 0 0 0
+(Feature)
+1 0
+(Motion)
+1 0
+T:
+(Shape_OUT)
+0 0 1 0 0 0 0 0 0
+(Color_OUT)
+0 0 1 0 0 0 0 0 0
+(Feature_OUT)
+1 0
+(Motion_OUT)
+1 0
+;
+
+name: FifthExample
+I:
+(Shape)
+0 0 0 0 0 0 1 0 0
+(Color)
+0 0 0 0 0 0 1 0 0
+(Feature)
+0 1
+(Motion)
+1 0
+T:
+(Shape_OUT)
+0 0 0 0 0 0 1 0 0
+(Color_OUT)
+0 0 0 0 0 0 1 0 0
+(Feature_OUT)
+0 1
+(Motion_OUT)
+1 0
+;
+
+name: SixthExample
+I:
+(Shape)
+0 0 0 0 1 0 0 0 0
+(Color)
+0 0 0 0 1 0 0 0 0
+(Feature)
+0 1
+(Motion)
+1 0
+T:
+(Shape_OUT)
+0 0 0 0 1 0 0 0 0
+(Color_OUT)
+0 0 0 0 1 0 0 0 0
+(Feature_OUT)
+0 1
+(Motion_OUT)
+1 0
+;
+
+name: SeventhExample
+I:
+(Shape)
+0 0 0 0 0 0 0 1 0
+(Color)
+0 0 0 0 0 0 0 1 0
+(Feature)
+1 0
+(Motion)
+1 0
+T:
+(Shape_OUT)
+0 0 0 0 0 0 0 1 0
+(Color_OUT)
+0 0 0 0 0 0 0 1 0
+(Feature_OUT)
+1 0
+(Motion_OUT)
+1 0
+;
+
+name: EighthExample
+I:
+(Shape)
+0 0 0 0 0 1 0 0 0
+(Color)
+0 0 0 0 0 1 0 0 0
+(Feature)
+0 1
+(Motion)
+0 1
+T:
+(Shape_OUT)
+0 0 0 0 0 1 0 0 0
+(Color_OUT)
+0 0 0 0 0 1 0 0 0
+(Feature_OUT)
+0 1
+(Motion_OUT)
+0 1
+;
+
